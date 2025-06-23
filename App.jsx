fd6bf93309b19ea6136d19d2e1435485ce1e6c2b
aleksangelov1 <aleks@marketedbyaa.com>
2025-06-23 16:57:53 +0100

    src

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '2rem' }}>
+      <h1>Welcome to Marketed by AA</h1>
+      <p>Your marketing partner.</p>
+    </div>
+  );
+}
+
+export default App;
